2933648c0036053f260935a359e275c0e3479197
Valentin Alvès <valentin-alves@hotmail.com>
2022-10-25 08:14:51 +0200

    added test cases for 3 and 4 books

--- src/main/kotlin/Potter.kt
+++ src/main/kotlin/Potter.kt
@@ -1,5 +1,7 @@
 private const val FIVE_PERCENT_DISCOUNT = 0.95
 
+private const val TEN_PERCENT_DISCOUNT = 0.90
+
 class Potter(private val books: List<String>) {
 
     fun calculateAmountToPay(): Double {
@@ -9,9 +11,12 @@ class Potter(private val books: List<String>) {
             return (8 * books.count()).toDouble()
         }
 
+        if (books.size == 2) return (8 * 2) * FIVE_PERCENT_DISCOUNT
 
-        if (books.size == 2) {
-            return (8 * 2) * FIVE_PERCENT_DISCOUNT
-        } else return 8.0
+        if (books.size == 3) return (8 * 3) * TEN_PERCENT_DISCOUNT
+
+        if (books.size == 4) return (8 * 4) * 0.80
+
+        return 8.0
     }
 }
--- src/test/kotlin/PotterTest.kt
+++ src/test/kotlin/PotterTest.kt
@@ -30,9 +30,35 @@ class PotterTest {
     }
 
     @Test
+    fun `buying three different books gets us a 10 percent discount` () {
+        // Arrange
+        val books = listOf("Harry Potter Book One", "Harry Potter Book Two", "Harry Potter Book Three")
+        val potter = Potter(books)
+
+        // Act
+        val amountToPay = potter.calculateAmountToPay()
+
+        // Assert
+        assertThat(amountToPay).isEqualTo(21.60)
+    }
+
+    @Test
+    fun `buying four different books gets us a 20 percent discount` () {
+        // Arrange
+        val books = listOf("Harry Potter Book One", "Harry Potter Book Two", "Harry Potter Book Three", "Harry Potter Book Four")
+        val potter = Potter(books)
+
+        // Act
+        val amountToPay = potter.calculateAmountToPay()
+
+        // Assert
+        assertThat(amountToPay).isEqualTo(25.60)
+    }
+
+    @Test
     fun `buying same books twice don't get us any discount` () {
         // Arrange
-        val books = mutableListOf("Harry Potter Book One", "Harry Potter Book One")
+        val books = listOf("Harry Potter Book One", "Harry Potter Book One")
         val potter = Potter(books)
 
         // Act
@@ -41,4 +67,19 @@ class PotterTest {
         // Assert
         assertThat(amountToPay).isEqualTo(16.0)
     }
+
+    @Test
+    fun `buying two identical sets gives us 5 percent on each set` () {
+        // Arrange
+        val books = listOf("Harry Potter Book One", "Harry Potter Book One", "Harry Potter Book Two", "Harry Potter Book Two")
+        val potter = Potter(books)
+
+        // Act
+        val amountToPay = potter.calculateAmountToPay()
+
+        // Assert
+        assertThat(amountToPay).isEqualTo(30.40)
+    }
+
+
 }
